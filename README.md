c3395ede12974b623299bfc9908d7e8b94a7b228
skyter68 <schitter@yahoo.com>
2019-02-07 14:23:16 +0100

    Corrected statistics_median.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # demo-python
 Exemples de code python
 - math_constants.py -> affiche quelques constantes mathématiques
+- statistics_median.py -> calcul de médianes
--- statistics_median.py
+++ statistics_median.py
@@ -7,3 +7,9 @@ data = [1, 2, 2, 5, 10, 12]
 print('median     : {:0.2f}'.format(median(data)))
 print('low        : {:0.2f}'.format(median_low(data)))
 print('high       : {:0.2f}'.format(median_high(data)))
+
+data = [10, 20, 30, 40]
+
+print('1: {:0.2f}'.format(median_grouped(data, interval=1)))
+print('2: {:0.2f}'.format(median_grouped(data, interval=2)))
+print('3: {:0.2f}'.format(median_grouped(data, interval=3)))
