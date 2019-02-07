eb5270ce24b1b61105788e2f0b8c0eab53c6f870
skyter68 <schitter@yahoo.com>
2019-02-07 14:17:59 +0100

    Add statistics_median.py

---
+++ statistics_median.py
@@ -0,0 +1,9 @@
+# statistics_median.py
+
+from statistics import *
+
+data = [1, 2, 2, 5, 10, 12]
+
+print('median     : {:0.2f}'.format(median(data)))
+print('low        : {:0.2f}'.format(median_low(data)))
+print('high       : {:0.2f}'.format(median_high(data)))
